11635b348e7f271947289e4c3fb0b3bf859278ec
Medrano Alexis Bruno Baloy <alexismedrano9139@gmail.com>
2020-06-05 23:50:07 -0300

    traer datos de una API

--- App.js
+++ App.js
@@ -1,47 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
-//conf FlatList os permite redactar listas dentro de la app
+
+
 export default function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    // effect//un efecto es una cosa que no sabemos que resultado tendra
+    // return () => {
+    //   cleanup
+    // }
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data)
+        setLoading(false)
+      })
+  }, [])
 
-
-
+  if (loading) {
+    return <View><Text style={styles.center}>Cargando...</Text></View>
+  }
   return (
     <View style={styles.container}>
+      {/* <Text>Cargado!!!</Text> */}
       <FlatList
-        data={[
-          { key: "1", name: "alexis" },
-          { key: "2", name: "bruno" },
-          { key: "3", name: "baloy" },
-          { key: "4", name: "medrano" },
-          { key: "5", name: "tania" },
-          { key: "6", name: "emilse" },
-          { key: "7", name: "alexis" },
-          { key: "8", name: "bruno" },
-          { key: "9", name: "baloy" },
-          { key: "10", name: "medrano" },
-          { key: "11", name: "tania" },
-          { key: "12", name: "emilse" },
-          { key: "13", name: "alexis" },
-          { key: "14", name: "bruno" },
-          { key: "15", name: "baloy" },
-          { key: "16", name: "medrano" },
-          { key: "17", name: "tania" },
-          { key: "18", name: "emilse" },
-          { key: "19", name: "alexis" },
-          { key: "20", name: "bruno" },
-          { key: "21", name: "baloy" },
-          { key: "22", name: "medrano" },
-          { key: "23", name: "tania" },
-          { key: "24", name: "emilse" },
-        ]}//arreglo
-        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}//explica como se renderizara el contenido de los datos que le pasemos en data
-      />
+        data={users}
+        renderItem={({ item }) => <Text>{item.name}     {item.email}</Text>}
+        keyExtractor={item => String(item.id)}
+      >
+      </FlatList>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   container: {
     flex: 1,//este representa todo ya que es el que envuelve a todo
     backgroundColor: '#fff',
@@ -49,11 +48,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingTop: 22
   },
-  item: {
-    padding: 10,
-    fontSize: 22,
-    height: 50,
-    borderBottomColor: "red",
-    borderBottomWidth: 1,
-  }
 });
